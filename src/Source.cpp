308aa30eb55e32741b27865141eb95d29f1ceb69
ChuzaWick420 <zaiddadev@gmail.com>
2023-12-04 16:51:45 +0500

    Removed build files
    used git rm -r --cached to removed the build files from repo

--- src/Source.cpp
+++ src/Source.cpp
@@ -1,7 +1,21 @@
 #include <SFML/Graphics.hpp>
 
-int main()
-{
+int main() {
+
+    sf::RenderWindow gameWindow(sf::VideoMode(800, 600), "Dodge-em!");
+
+    while(gameWindow.isOpen()) {
+        sf::Event event;
+        while(gameWindow.pollEvent(event)) {
+            if(event.type == sf::Event::Closed) {
+                gameWindow.close();
+            }
+        }
+
+        gameWindow.clear(sf::Color::Black);
+        gameWindow.display();
+
+    }
 
     return 0;
 }
