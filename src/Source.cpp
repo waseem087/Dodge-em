0a00ae03e19d871806e475ee52514acfdae76f5b
ChuzaWick420 <zaiddadev@gmail.com>
2023-12-04 17:59:26 +0500

    refactored game class
    - Removed unnecessary code
    - Made the class more modular
    - Update handles rendering at defined frames per seconds
    - handleEvents handles inputs

--- src/Source.cpp
+++ src/Source.cpp
@@ -1,21 +1,10 @@
 #include <SFML/Graphics.hpp>
+#include "game.h"
 
 int main() {
 
-    sf::RenderWindow gameWindow(sf::VideoMode(800, 600), "Dodge-em!");
-
-    while(gameWindow.isOpen()) {
-        sf::Event event;
-        while(gameWindow.pollEvent(event)) {
-            if(event.type == sf::Event::Closed) {
-                gameWindow.close();
-            }
-        }
-
-        gameWindow.clear(sf::Color::Black);
-        gameWindow.display();
-
-    }
+    Game game;
+    game.start_game();
 
     return 0;
 }
---
+++ src/game.cpp
@@ -0,0 +1,46 @@
+#include "game.h"
+
+Game::Game() {
+    bg_texture.loadFromFile("../assets/img/background.jpg");
+    background.setTexture(bg_texture);
+    background.setScale(1.25, 1.4);
+}
+
+void Game::update(sf::RenderWindow& target_w) {
+    target_w.clear(sf::Color::Black);
+    target_w.draw(background);
+    target_w.display();
+}
+
+void Game::handleEvents(sf::Event& e, sf::RenderWindow& target_w) {
+    switch (e.type) {
+        case sf::Event::Closed:
+            target_w.close();
+            break;
+        default:
+            break;
+    }
+}
+
+void Game::start_game() {
+
+    sf::RenderWindow gameWindow(sf::VideoMode(1000, 700), title);
+
+    sf::Clock ticks;
+    float fps = 60;
+
+    while (gameWindow.isOpen()) {
+
+        float timer = ticks.getElapsedTime().asMilliseconds();
+        sf::Event e;
+
+        while (gameWindow.pollEvent(e)) {
+            handleEvents(e, gameWindow);
+        }
+
+        if (timer >= 1.0 / fps * 1000) {
+            update(gameWindow);
+            ticks.restart();
+        }
+    }
+}
